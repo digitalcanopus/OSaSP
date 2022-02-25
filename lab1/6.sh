ef32697448098d3030746090ae957155a8bce7f3
digitalcanopus <100073360+digitalcanopus@users.noreply.github.com>
2022-02-25 21:12:07 +0300

    Update 6.sh

--- lab1/6.sh
+++ lab1/6.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ ! "$1" -o ! "$2" -o ! "$3" ]; then
+if [ ! $1 -o ! $2 -o ! $3 ]; then
 	echo "Not enough parameters: 'output file name' 'search dir name' 'extention'"
-elif [ ! -d  "$2" ]; then
+elif [ ! -d  $2 ]; then
 	echo "Invalid parameter: no such directory." >&2
 else
-	(find "$2" -type f -name "*.$3" | sort -i) > "$1"
+	(find $2 -type f -name "*.$3" | sort -i) > $1
 fi
