2be2bde149c29a7154a00ff049f494648ec78c98
digitalcanopus <100073360+digitalcanopus@users.noreply.github.com>
2022-03-31 21:57:43 +0300

    Create 5.c

---
+++ lab2/5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <dirent.h>
+int entdir(char const *name){
+	int i = 1;
+	DIR *dir = opendir(name);
+	if (!name){
+		perror("Open directory trouble");
+		return 1;
+	}
+	struct dirent *d;
+	while (d = readdir(dir)){
+		printf("%d. %s\n", i++, d->d_name);
+	}
+	closedir(dir);
+	return 0;
+}
+int main(int argc, char **argv){
+	printf("Current directory: \n");
+	entdir(".");
+	printf("Root directory: \n");
+	entdir("/");
+	return 0;
+}
