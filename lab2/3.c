d140b2396fb2ffbf7c2b78fb794856be74a32aaf
digitalcanopus <100073360+digitalcanopus@users.noreply.github.com>
2022-04-01 21:38:25 +0300

    Update 3.c

--- lab2/3.c
+++ lab2/3.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
+#include <ctype.h>
 int main (int argc, char **argv){
+	if (argc == 3){
 	char sym;
 	int count = 0;
+	int strflag = 0;
 	int  N = strtol(argv[2], NULL, 10);
-	if (N < 0){
-		perror("Invalid string");
-		return 1;
+	for (int i = 0; argv[2][i] != '\0'; i++){
+		if (!isdigit(argv[2][i]))
+			strflag = 1;
+	}
+	if (strflag == 1){
+		fprintf(stderr, "Incorrect string format\n");
+		return 0;
 	}
 	FILE *f;
 	f = fopen(argv[1], "r");
 	if (f == NULL){
-		perror("Open file error");
+		fprintf(stderr, "Open file error");
 		return 1;
 	}
 	while ((N == 0) || (count < N)){
-		do{
+		do {
 			sym = fgetc(f);
 			if (sym == EOF)
 				break;
 			printf("%c", sym);
-		}while (sym != '\n');
+		} while (sym != '\n');
 		if (sym == EOF)
 			break;
 		count++;
@@ -30,5 +39,11 @@ int main (int argc, char **argv){
 		}
 	}
 	fclose(f);
+	}
+	else{
+		fprintf(stderr, "incorrect format: file-name, strings quantity\n");
+		return 1;
+	}
+	
 	return 0;
 }
