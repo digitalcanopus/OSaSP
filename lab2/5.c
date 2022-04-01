e8746009389e4d4ec03c2fa2fe857e57c3b8e041
digitalcanopus <100073360+digitalcanopus@users.noreply.github.com>
2022-04-01 21:43:31 +0300

    Update 5.c

--- lab2/5.c
+++ lab2/5.c
@@ -4,11 +4,11 @@ int entdir(char const *name){
 	int i = 1;
 	DIR *dir = opendir(name);
 	if (!name){
-		perror("Open directory trouble");
+		fprintf(stderr, "Open directory trouble");
 		return 1;
 	}
 	struct dirent *d;
-	while (d = readdir(dir)){
+	while ((d = readdir(dir))){
 		printf("%d. %s\n", i++, d->d_name);
 	}
 	closedir(dir);
